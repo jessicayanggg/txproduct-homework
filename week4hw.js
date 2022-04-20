6a8153dcae0ff86620ec13e961c0ac4f7041d5de
Jessica Yang <jessicayang9992@gmail.com>
2022-04-20 12:04:23 -0500

    week4hwupdate

--- week4hw.js
+++ week4hw.js
@@ -1,20 +1,16 @@
 function powersOfThree() {
-    for(var i = 1; i < 10; i++) {
-        powerThree = 3 * ...
-        console.log(powerThree);
-
-        if(powerThree == 27) {
+    for(var i = 1; i < 19683; i *= 3) {
+        console.log(i);
+        if(i == 27) {
             console.log("This is my favorite power of three!");
         }
     }
 }
-
-var timeoutID = setTimeout(powersOfThree, 3000);
+setTimeout(powersOfThree, 3000);
 
 function everyTenSeconds() {
-    console.log("Hello World!");
-
+    var intervalId = setInterval(everyTenSeconds, 10000, "Hello World!");
+    setTimeout(clearInterval, 130000, intervalId);
 }
+everyTenSeconds();
 
-var intervalId = setInterval(everyTenSeconds, 10000);
-setTimeout(clearInterval, 200000, intervalId);
